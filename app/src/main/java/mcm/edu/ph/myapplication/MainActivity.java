0746286f423d92e231d30b0345ad521861e118e6
JayGamao <jonreff.gamao@yahoo.com>
2022-05-05 10:39:40 +0800

    g

--- app/src/main/java/mcm/edu/ph/myapplication/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/myapplication/MainActivity.java
@@ -16,16 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lol);
         bleh = findViewById(R.id.answer);
-        var1 = 2;
-        var2 = 4;
-        var3 = 6;
-        var4 = 8;
-        var5 = 10;
 
-
-        ThirdClass op = new ThirdClass();
-        ThirdClass op1 = new ThirdClass(var1, var2, var3,var4,var5);
-        op1.Operation1(op1.var1, op1.var2, op1.var3, op1.var4);
-        bleh.setText(String.valueOf(op1.var2));
+        bleh.setText(String.valueOf());
     }
 }
